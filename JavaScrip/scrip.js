ed1bb1ef64ebf4af3cc413e5bd50089decc9e8df
diego9702 <diegosnieto97@gmail.com>
2020-10-18 22:50:00 -0500

    primer comit

---
+++ JavaScrip/scrip.js
@@ -0,0 +1,23 @@
+alert("Bienvenidos disfruta del contenido y buenas vibras para ti ");
+//prompt("ingresa el nombre del cantante")
+//confirm("deseas salir de la pagina?")
+
+//var nombre="ROBERT NESTA MARLEY :) ";
+alert(nombre)
+//var numero= 1;
+alert(numero)
+//var ejemplo= false;
+alert(ejemplo)
+
+
+function mensajeArtista() {
+
+var mensajeArtista=prompt("Como se llama este cantante?")
+
+if (mensajeArtista=="robert nesta marley") { 
+    alert("Este nombre es Correcto, disfrutate del buen reggae y tomalo suave ¡¡...")
+} else {
+    alert("Este nombre es Incorrecto")
+}
+}
+
